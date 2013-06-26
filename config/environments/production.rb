b7a60c27e9740b76fb0b7b36d1af72281455a1b3
Clinton Judy <clinton@j-udy.com>
2013-06-26 00:25:37 -0400

    Trying other settings.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,11 +33,11 @@ Discourse::Application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       :address   => "smtp.mandrillapp.com",
-      :port      => 25,
+      :port      => 587,
       :enable_starttls_auto => true,
       :user_name =>      ENV['MANDRILL_USERNAME'],
       :password =>       ENV['MANDRILL_APIKEY'],
-      :domain =>         'themakespace.com',
+      :domain =>         'makespace.herokuapp.com',
       :authentication => :login
   }
 
